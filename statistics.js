b06e31181c3945e0cd95620bbe88e5a3db4ccc95
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-03-16 18:40:00 -0400

    added request for activities

--- statistics.js
+++ statistics.js
@@ -1,6 +1,6 @@
-server_url = 'http://68.39.46.187:50000/';
+server_url = 'http://68.39.46.187:50000/GreenwayCap/DataRelay.svc/Activity/szook';
 
-function ping() {
+function getActivities() {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		console.log(xmlhttp.responseText);
