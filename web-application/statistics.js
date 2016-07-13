f2f6a90837c990e2dca33677b6af959886ac0299
chadwell1028 <chadwell1028@gmail.com>
2016-07-12 21:36:37 -0400

    Initial commit for Master page and some formatting.

---
+++ web-application/statistics.js
@@ -0,0 +1,10 @@
+server_url = 'http://68.39.46.187:50000/GreenwayCap/DataRelay.svc/Activity/szook';
+
+function getActivities() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        console.log(xmlhttp.responseText);
+    }
+    xmlhttp.open("GET", server_url, true);
+    xmlhttp.send();
+}
