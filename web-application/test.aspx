32d0ca825d80fa6e53e50f3e1ee82c0aa582cb2f
chadwell1028 <chadwell1028@gmail.com>
2016-10-12 16:43:06 -0400

    Issues connecting to Azure DB. Pushing code for Brenden to do some troubleshooting.

---
+++ web-application/test.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="web_application.test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentBody" runat="server">
+    <input type="button" id="btn" value="submit" 
+        OnServerClick="DBConnectTest"
+       runat="server" />
+
+    <div>Accounts:
+        
+        <%=sqlData%></div>
+</asp:Content>
---
+++ web-application/test.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Configuration;
+using ConfigurationSettings = System.Configuration.ConfigurationManager;
+using System.Data.SqlClient;
+using System.Web.Security;
+using System;
+using System.Web.UI;
+
+namespace web_application
+{
+    public partial class test : System.Web.UI.Page
+    {
+
+        public string sqlData;
+        protected void Page_Load()
+        {
+
+        }
+
+        protected void DBConnectTest(object sender, EventArgs e)
+        {
+            string constr = @"Data Source = WIN-ME4L0HSF3UK; Initial Catalog = Greenway; Integrated Security = True";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM dbo.Account";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    
+
+                    con.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    // Data is accessible through the DataReader object here.
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            sqlData = reader.GetString(1);
+                        }
+                    }
+
+                    con.Close();
+                }
+            }
+        }
+    }
+}
