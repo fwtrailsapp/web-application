7f16f33ca78fc907c55dae520f1188b264339a84
bfullenkamp95 <fullb01@students.ipfw.edu>
2016-10-17 22:30:16 -0400

    sql connection
    works on server but not in visual studio

--- web-application/test.aspx.cs
+++ web-application/test.aspx.cs
@@ -19,31 +19,36 @@ namespace web_application
 
         protected void DBConnectTest(object sender, EventArgs e)
         {
-            string constr = @"Data Source = WIN-ME4L0HSF3UK; Initial Catalog = Greenway; Integrated Security = True";
+            ConnectionStringSettings settings;
+            settings = System.Configuration.ConfigurationManager.ConnectionStrings["GreenwayConnection"];
+            string constr = settings.ConnectionString;
+
             using (SqlConnection con = new SqlConnection(constr))
+
+            using (SqlCommand cmd = new SqlCommand())
             {
-                using (SqlCommand cmd = new SqlCommand())
+                cmd.CommandText = "SELECT * FROM dbo.Account";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+
+
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                // Data is accessible through the DataReader object here.
+
+                if (reader.HasRows)
                 {
-                    cmd.CommandText = "SELECT * FROM dbo.Account";
-                    cmd.CommandType = CommandType.Text;
-                    cmd.Connection = con;
-                    
-
-                    con.Open();
-
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    // Data is accessible through the DataReader object here.
-
-                    if (reader.HasRows)
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        for (int i = 0; i < reader.FieldCount; i++)
                         {
-                            sqlData = reader.GetString(1);
+                            sqlData += reader.GetValue(i).ToString();
                         }
                     }
-
-                    con.Close();
                 }
+
+                con.Close();
             }
         }
     }
