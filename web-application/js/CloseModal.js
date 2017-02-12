e8145ddd1a07c76147710ee972f87f38df025240
Justin Chadwell <chadwell1028@gmail.com>
2017-02-11 19:32:18 -0500

    Trying a different Post method.

--- web-application/js/CloseModal.js
+++ web-application/js/CloseModal.js
@@ -1,35 +1,41 @@
-﻿$(document).ready(function ()
-{
+﻿$(document).ready(function () {
     var ticketId;
 
     $('#closeDialog').dialog({
         'buttons': {
             'Close': {
                 text: 'Close',
-                click: function ()
-                {
+                click: function () {
                     // Call Web API
-                    $.ajax({
-                        type: 'Post',
-                        contentType: "application/json; charset=utf-8",
-                        url: "http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close",  //method Name 
-                        data: JSON.stringify({ id: ticketId }),
-                        dataType: 'json',
-                        success: alert("success"), // callback above
-                        error: function (msg)
-                        {
-                            debugger;
-                            alert("error: " + msg.status);
-                        }
-                    });
+                    //$.ajax({
+                    //    type: 'Post',
+                    //    beforeSend: function (request) {
+                    //        request.setRequestHeader("Trails-Api-Key", ab83920bdc826bdaf);
+                    //    },
+                    //    contentType: "application/json; charset=utf-8",
+                    //    url: "http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close",  //method Name 
+                    //    data: JSON.stringify({ id: ticketId }),
+                    //    dataType: 'json',
+                    //    success: alert("success"), // callback above
+                    //    error: function (msg)
+                    //    {
+                    //        debugger;
+                    //        alert("error: " + msg.status);
+                    //    }
+                    //});
+
+                    $.post('http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close', { id: ticketId },
+                        function (returnedData) {
+                            console.log(returnedData);
+                        });
+
                     $(this).dialog("close");
 
                 }
             },
             'Cancel': {
                 text: 'Cancel',
-                click: function ()
-                {
+                click: function () {
                     $(this).dialog("close");
                 }
             },
@@ -40,8 +46,7 @@
         modal: true
     });
 
-    $('[id^=CloseButton]').click(function (e)
-    {
+    $('[id^=CloseButton]').click(function (e) {
         ticketId = this.id.replace("CloseButton", "");
         document.getElementById("closeDialog").innerHTML = "Are you sure you want to close ticket #" + ticketId + "?";
         e.preventDefault();
