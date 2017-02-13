3cbb421190d3e9a1b741868b0aaa6e60726f79f1
Justin Chadwell <chadwell1028@gmail.com>
2017-02-13 12:52:01 -0500

    Minor ajax change.

--- web-application/js/CloseModal.js
+++ web-application/js/CloseModal.js
@@ -7,27 +7,28 @@
                 text: 'Close',
                 click: function () {
                     // Call Web API
-                    //$.ajax({
-                    //    type: 'Post',
-                    //    beforeSend: function (request) {
-                    //        request.setRequestHeader("Trails-Api-Key", ab83920bdc826bdaf);
-                    //    },
-                    //    contentType: "application/json; charset=utf-8",
-                    //    url: "http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close",  //method Name 
-                    //    data: JSON.stringify({ id: ticketId }),
-                    //    dataType: 'json',
-                    //    success: alert("success"), // callback above
-                    //    error: function (msg)
-                    //    {
-                    //        debugger;
-                    //        alert("error: " + msg.status);
-                    //    }
-                    //});
+                    $.ajax({
+                        type: 'Post',
+                        crossDomain: true,
+                        beforeSend: function (request) {
+                            request.setRequestHeader("Trails-Api-Key", ab83920bdc826bdaf);
+                        },
+                        contentType: "application/json; charset=utf-8",
+                        url: "http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close",  //method Name 
+                        data: JSON.stringify({ id: ticketId }),
+                        dataType: 'jsonp',
+                        success: alert("success"), // callback above
+                        error: function (msg)
+                        {
+                            debugger;
+                            alert("error: " + msg.status);
+                        }
+                    });
 
-                    $.post('http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close', { id: ticketId },
-                        function (returnedData) {
-                            console.log(returnedData);
-                        });
+                    //$.post('http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close', { id: ticketId },
+                    //    function (returnedData) {
+                    //        console.log(returnedData);
+                    //    });
 
                     $(this).dialog("close");
 
