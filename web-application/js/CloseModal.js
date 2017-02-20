1bcbfe8707080a2231faf56c7cfb0cc6a5409518
Justin Chadwell <chadwell1028@gmail.com>
2017-02-20 11:46:29 -0500

    Close API call now works!

--- web-application/js/CloseModal.js
+++ web-application/js/CloseModal.js
@@ -8,21 +8,17 @@
                 click: function () {
                     // Call Web API
                     $.ajax({
-                        type: 'Get',
+                        type: 'POST',
                         crossDomain: true,
-                        beforeSend: function (request) {
-                            request.setRequestHeader("Trails-Api-Key", ab83920bdc826bdaf);
-                        },
                         contentType: "application/json; charset=utf-8",
                         url: "http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close",  //method Name 
                         data: JSON.stringify({ id: ticketId }),
-                        dataType: 'jsonp',
-                        success: alert("success"), // callback above
-                        error: function (msg)
-                        {
-                            debugger;
-                            alert("error: " + msg.status);
-                        }
+                        dataType: 'json',
+                        //success: alert("success"), // callback above
+                        //error: function (msg) {
+                        //    debugger;
+                        //    alert("error: " + msg.status);
+                        //}
                     });
 
                     //$.post('http://23.97.29.252:50000/capstone/datarelay.svc/trails/api/1/ticket/close', { id: ticketId },
