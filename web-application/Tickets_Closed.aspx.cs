4621ab261ee1fe338d2d4c65b700adfb5c1f5938
chadwell1028 <chadwell1028@gmail.com>
2017-02-28 13:34:42 -0500

    Changed GPS code on closed Tickets.

--- web-application/Tickets_Closed.aspx.cs
+++ web-application/Tickets_Closed.aspx.cs
@@ -75,7 +75,7 @@ namespace web_application
                                 commentId = "CommentButton" + id;
                                 imgId = "Image" + id;
                                 gpsId = "GPS" + id;
-                                gpsLink = "https://www.google.com/maps/place/" + longitude + " " + latitude;
+                                gpsLink = "https://www.google.com/maps/place/" + latitude + "," + longitude;
 
                                 closedTicketList = string.Concat(closedTicketList,
                                    $"<div id=\"ticketDiv\">"
